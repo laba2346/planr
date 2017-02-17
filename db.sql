cf4d3aac7bd699bf46c11ff7f34a5932be7c8187
Landon <laba@colorado.edu>
2017-02-17 16:21:52 -0700

    Added first three tables to database

--- db.sql
+++ db.sql
@@ -1,27 +1,18 @@
+create table if not exists `users` (
+`id` bigint(1) not null auto_increment,
+`name` varchar(200) not null,
+`email` varchar(50) not null,
+`password` varchar(512) not null,
+ primary key (`id`)
+);
 
+create table if not exists `assignments` (
+`id` bigint(1) not null auto_increment,
+`class_id` bigint(1),
+ primary key (`id`)
+);
 
-
-
-
-
-
-
-
-
-
-
---------------------------------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-
----------------------------------------------------------
+create table if not exists `contributors` (
+`user_id` bigint(1) not null,
+`assignment_id` bigint(1) not null
+);
