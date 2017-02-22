7be2973839a86dabeb65e271085801131a932800
gottacatchenall <michael@engr2-2-76-186-dhcp.int.colorado.edu>
2017-02-21 18:18:25 -0700

    package.json update

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 import Express from 'express';
+import { Provider, store } from 'redux';
 
 const app = Express();
 const port = 3000;
 
+// Wait on api calls here
+
+// Create new store and send initial state to client
 app.get('/*', (req,res) => {
     res.send('hello world!');
 });
