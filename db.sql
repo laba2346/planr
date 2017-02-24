206521d58e7e616e5881156439c5bbb3289b5535
Joe Los <jlos484@gmail.com>
2017-02-24 16:44:59 -0700

    got rid of sql constraints

--- db.sql
+++ db.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS users (
 CREATE TABLE IF NOT EXISTS classes (
     class_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     owner_id BIGINT UNSIGNED NOT NULL,
-    constraint constraint_1 FOREIGN KEY (owner_id) references users(user_id) on update cascade on delete cascade,
+    FOREIGN KEY (owner_id) references users(user_id) on update cascade on delete cascade,
     class_name VARCHAR(60) NOT NULL,
     class_info VARCHAR(1000) NULL,
     class_times VARCHAR(1000) NOT NULL /* THIS'LL PROBABLY BE A FORMATTED STRING WITH DAYS AND TIMES*/
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS classes (
 CREATE TABLE IF NOT EXISTS assignments (
     assignment_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     class_id BIGINT UNSIGNED NOT NULL,
-    constraint contraint_2 FOREIGN KEY (class_id) REFERENCES classes(class_id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (class_id) REFERENCES classes(class_id) ON UPDATE CASCADE ON DELETE CASCADE,
     assignment_name VARCHAR(100) NOT NULL,
     assignment_description VARCHAR(1000) NULL,
     assignment_due DATE NOT NULL
