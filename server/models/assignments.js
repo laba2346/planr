414d5b3e71ee31214569b4d2bc285b8bb889841d
Landon <laba2346@colorado.edu>
2017-02-24 17:42:10 -0700

    Didsies the fixsies

--- server/models/assignments.js
+++ server/models/assignments.js
@@ -1,22 +1,22 @@
 var assignments = sequelize.define('assignments', {
-  id: {
-    type: Sequelize.BIGINT
-  }
-  class_id: {
-    type: Sequelize.BIGINT
-  }
-  assignment_name: {
-    type: Sequelize.STRING
-  }
-  assignment_description: {
-    type: Sequelize.STRING
-  }
-  assignment_due {
-    type: Sequelize.DATE
-  }
+    id: {
+        type: Sequelize.BIGINT
+    },
+    class_id: {
+        type: Sequelize.BIGINT
+    },
+    assignment_name: {
+        type: Sequelize.STRING
+    },
+    assignment_description: {
+        type: Sequelize.STRING
+    },
+    assignment_due {
+        type: Sequelize.DATE
+    },
 }, {
-  freezeTableName: true,
-  timestamps: false
+    freezeTableName: true,
+    timestamps: false
 });
 
 export default assignments;
