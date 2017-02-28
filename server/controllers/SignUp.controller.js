e002186607bcb165aebc987adc6e21bba2617386
gottacatchenall <mdcatchen@gmail.com>
2017-02-28 15:57:46 -0700

    Adds new user with hashed password to database

--- server/controllers/SignUp.controller.js
+++ server/controllers/SignUp.controller.js
@@ -1,15 +1,16 @@
 // Import model 
 var models = require('../models');
+import bcrypt from 'bcrypt';
 
 export function newUser(req, res){
     var username = req.body.username;
     var password = req.body.password;
+    var hash = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
     var email = req.body.email;
-
     models.sequelize.sync().then(function(){
         models.users.create({
             username: username,
-            password: password,
+            password: hash,
             email: email,
         });
     });
