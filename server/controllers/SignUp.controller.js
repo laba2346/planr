301296ca36df5056a70c173ff5f37acece62dc57
gottacatchenall <mdcatchen@gmail.com>
2017-02-28 15:31:13 -0700

    Simple adding into database

--- server/controllers/SignUp.controller.js
+++ server/controllers/SignUp.controller.js
@@ -1,6 +1,17 @@
 // Import model 
+var models = require('../models');
 
 export function newUser(req, res){
-    console.log(req.body);
+    var username = req.body.username;
+    var password = req.body.password;
+    var email = req.body.email;
+
+    models.sequelize.sync().then(function(){
+        models.users.create({
+            username: username,
+            password: password,
+            email: email,
+        });
+    });
     res.end();
 }
