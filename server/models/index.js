55c273e4bebf2d6bc001da1eeb9b980870d1a538
gottacatchenall <mdcatchen@gmail.com>
2017-03-14 16:44:56 -0600

    quick db fix

--- server/models/index.js
+++ server/models/index.js
@@ -3,29 +3,30 @@
 var fs        = require('fs');
 var path      = require('path');
 var Sequelize = require('sequelize');
-var basename  = path.basename(__filename);
 var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/dbConfig.json');
 var db        = {};
 
-var sequelize = new Sequelize(config.database, config.username, config.password, config);
-/*
-fs
-  .readdirSync(__dirname)
-  .filter(function(file) {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(function(file) {
-    var model = sequelize['import'](path.join(__dirname, file));
-    db[model.name] = model;
-  });
+if (process.env.DATABASE_URL) {
+  var sequelize = new Sequelize(process.env.DATABASE_URL,config);
+} else {
+  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+var assignments = require('./assignments.js');
+var classes = require('./classes.js');
+var users = require('./users.js');
+
+db[assignments] = assignments;
+db[classes] = classes;
+db[users] = users;
 
 Object.keys(db).forEach(function(modelName) {
-  if (db[modelName].associate) {
+  if ("associate" in db[modelName]) {
     db[modelName].associate(db);
   }
 });
-*/
+
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
