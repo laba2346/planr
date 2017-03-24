0328cc1cbf4f3563c1b244cb1ec9729d10a26888
gottacatchenall <mdcatchen@gmail.com>
2017-03-24 14:56:01 -0600

    this is kind of a mess 🙃

--- client/modules/CreateAssignment/components/NewAssignmentForm/NewAssignmentForm.js
+++ client/modules/CreateAssignment/components/NewAssignmentForm/NewAssignmentForm.js
@@ -1,12 +1,13 @@
 import React, { PropTypes, Component } from 'react';
 import { connect } from 'react-redux';
+import Datetime from 'react-datetime';
 import styles from './NewAssignmentForm.css';
 
 class NewAssignmentForm extends Component {
 
     constructor(props){
         super(props);
-        this.state = {username: '', password: '', email: '' };
+        this.state = {name: '', desc: '', date: '' };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
@@ -32,10 +33,11 @@ class NewAssignmentForm extends Component {
                 <form className={styles['new-assignment-form']} onSubmit={this.handleSubmit}>
                     <label> New Assignment </label>
                     {this.props.failedLogin && <div className={styles['login-failed']}>!</div>}
-                    <input name="email" className={styles['input']} type="text" placeholder="Email" value={this.state.email} onChange={this.handleChange} />
-                    <input name="password" className={styles['input']} type="password" placeholder="Password" value={this.state.password} onChange={this.handleChange} />
+                    <input name="name" className={styles['input']} type="text" placeholder="Name" value={this.state.name} onChange={this.handleChange} />
+                    <input name="description" className={styles['input']} type="text" placeholder="Description" value={this.state.data} onChange={this.handleChange} />
                     <input type="submit" className={styles['login-submit'] + ' transition'} value="Submit" />
                 </form>
+                <Datetime />
             </div>
         );
   }
