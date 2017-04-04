003d4910b5897813ca3e8c9c21aaa4fe1fe5c938
gottacatchenall <mdcatchen@gmail.com>
2017-04-03 19:09:03 -0600

    testing framework

---
+++ client/modules/Container/__tests__/Container.spec.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import test from 'ava';
+import sinon from 'sinon';
+import { shallow, mount } from 'enzyme';
+
+test('foo', t => {
+	t.pass();
+});
---
+++ client/modules/Container/__tests__/ContainerReducer.spec.js
@@ -0,0 +1,6 @@
+import test from 'ava';
+import { reducerTest } from 'redux-ava';
+
+test('foo', t => {
+	t.pass();
+});
