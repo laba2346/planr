befc485f53fbf2bc1789a7547dc4bb250c5aa404
Landon <laba2346@colorado.edu>
2017-04-04 19:02:32 -0600

    PLS WERK

--- client/modules/Container/__tests__/AppActions.spec.js
+++ client/modules/Container/__tests__/AppActions.spec.js
@@ -1,5 +1,6 @@
 import test from 'ava';
 import { actionTest } from 'redux-ava';
-import { TOGGLE_ADD_POST, toggleAddPost } from '../AppActions';
 
-test('should return the correct type for toggleAddPost', actionTest(toggleAddPost, null, { type: TOGGLE_ADD_POST }));
+test(t => {
+	t.deepEqual([1, 2], [1, 2]);
+});
