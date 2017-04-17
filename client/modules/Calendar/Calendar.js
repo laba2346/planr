6ec4fafbdb4c838b94b26c14219bcc0623520ac3
gottacatchenall <mdcatchen@gmail.com>
2017-04-17 13:17:58 -0600

    calendar update

--- client/modules/Calendar/Calendar.js
+++ client/modules/Calendar/Calendar.js
@@ -1,21 +1,27 @@
 import React, { PropTypes, Component } from 'react';
 import { connect } from 'react-redux';
 import styles from './Calendar.css';
+import BigCalendar from 'react-big-calendar';
+import events from './events.js'
+import moment from 'moment'
 
 class Calendar extends Component {
 
     constructor () {
         super();
         this.state = {}
+        BigCalendar.momentLocalizer(moment);
     }
 
-    render() {
+    render(){
         return (
-            <div className={styles['calendar-container']}>
-                Calendar view
-            </div>
-        );
-  }
+        <BigCalendar
+          {...this.props}
+          events={events}
+          defaultDate={new Date(2015, 3, 1)}
+        />
+        )
+    }
 }
 
 // Retrieve data from store as props
