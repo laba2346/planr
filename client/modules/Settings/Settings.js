35e991ac6b7d50d5c5331ff06deba6f721e20e25
Landon <laba2346@colorado.edu>
2017-04-18 14:32:52 -0600

    New look for settings

--- client/modules/Settings/Settings.js
+++ client/modules/Settings/Settings.js
@@ -7,7 +7,7 @@ class Settings extends Component {
 
     constructor () {
         super();
-        this.state = {color: ''};
+        this.state = {color: '', username: '', password1: '', password2: '', email: ''};
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
@@ -34,10 +34,14 @@ class Settings extends Component {
     render() {
         return (
             <div>
-            <form className={styles['sign-up-form']} onSubmit={this.handleSubmit}>
-                <label> Color Theme </label>
+            <form className={styles['settings-form']} onSubmit={this.handleSubmit}>
+                <label> change settings </label>
                 <input name="color" className={styles['valid-field'] + ' ' + styles['input']} type="text" placeholder="Color" value={this.state.color} onChange={this.handleChange} />
-                <input type="submit" className={styles['sign-up-submit'] + ' transition'} value="Submit" />
+                <input name="username" className={styles['valid-field'] + ' ' + styles['input']} type="text" placeholder="Change username" value={this.state.username} onChange={this.handleChange} />
+                <input name="email" className={styles['valid-field'] + ' ' + styles['input']} type="text" placeholder="Change email" value={this.state.email} onChange={this.handleChange} />
+                <input name="password1" className={styles['valid-field'] + ' ' + styles['input']} type="password" placeholder="Change password" value={this.state.password1} onChange={this.handleChange} />
+                <input name="password2" className={styles['valid-field'] + ' ' + styles['input']} type="password" placeholder="Verify new password" value={this.state.password2} onChange={this.handleChange} />
+                <input type="submit" className={styles['settings-submit'] + ' transition'} value="Submit" />
             </form>
             </div>
         );
