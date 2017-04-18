ddc4c0350bfbd20c7577291e7998ac0ab6da80c9
Landon <laba2346@colorado.edu>
2017-04-18 14:56:43 -0600

    Added color picker. State changes with color selection. Modified some css

--- client/modules/Settings/Settings.js
+++ client/modules/Settings/Settings.js
@@ -2,6 +2,7 @@ import React, { PropTypes, Component } from 'react';
 import { connect } from 'react-redux';
 import styles from './Settings.css';
 import { changeSettingRequest } from './SettingsActions.js'
+import { CirclePicker } from 'react-color';
 
 class Settings extends Component {
 
@@ -9,6 +10,7 @@ class Settings extends Component {
         super();
         this.state = {color: '', username: '', password1: '', password2: '', email: ''};
         this.handleChange = this.handleChange.bind(this);
+        this.handleChangeColor = this.handleChangeColor.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
@@ -29,19 +31,23 @@ class Settings extends Component {
           [name]: value
         });
     }
+    handleChangeColor = (color) => {
+        console.log(color.hex)
+        this.setState({ color: color.hex });
+    };
 
 
     render() {
         return (
             <div>
             <form className={styles['settings-form']} onSubmit={this.handleSubmit}>
-                <label> change settings </label>
-                <input name="color" className={styles['valid-field'] + ' ' + styles['input']} type="text" placeholder="Color" value={this.state.color} onChange={this.handleChange} />
+                <label className={styles['settings-label']}> select themes color </label>
+                <CirclePicker circleSpacing='10'color={this.state.color} onChangeComplete={this.handleChangeColor} />
                 <input name="username" className={styles['valid-field'] + ' ' + styles['input']} type="text" placeholder="Change username" value={this.state.username} onChange={this.handleChange} />
                 <input name="email" className={styles['valid-field'] + ' ' + styles['input']} type="text" placeholder="Change email" value={this.state.email} onChange={this.handleChange} />
                 <input name="password1" className={styles['valid-field'] + ' ' + styles['input']} type="password" placeholder="Change password" value={this.state.password1} onChange={this.handleChange} />
                 <input name="password2" className={styles['valid-field'] + ' ' + styles['input']} type="password" placeholder="Verify new password" value={this.state.password2} onChange={this.handleChange} />
-                <input type="submit" className={styles['settings-submit'] + ' transition'} value="Submit" />
+                <input type="submit" className={styles['settings-submit'] + ' transition'} value="Save" />
             </form>
             </div>
         );
